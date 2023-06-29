5b3db95fe1316a6d75f3b35be39b6f65d55c2522
hjee02018@naver.com <hjee02018@naver.com>
2023-06-29 16:42:07 +0900

    [ADD] : initialize git

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import os
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/<pdb_id>')
+def viewer1(pdb_id):
+    pdb_filename = f'{pdb_id.upper()}.pdb'
+    pdb_url = url_for('static', filename='pdbs/' + pdb_filename)
+    return render_template('browse_complex.html', data_href=pdb_url)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
