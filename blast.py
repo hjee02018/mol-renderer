3d753fd6a2667786dcc48fca0d83b1961dd02b36
hjee02018@naver.com <hjee02018@naver.com>
2023-07-03 16:42:44 +0900

    [FIX] : 서버 응답 수정

--- blast.py
+++ blast.py
@@ -1,17 +1,25 @@
-import os
-from flask import Flask, render_template, url_for
+import subprocess
+from flask import Flask, render_template, request
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
-@app.route('/<pdb_id>')
-def viewer1(pdb_id):
-    pdb_filename = f'{pdb_id.upper()}.pdb'
-    pdb_url = url_for('static', filename='pdbs/' + pdb_filename)
-    return render_template('browse_complex.html', data_href=pdb_url)
-
-@app.route('/')
-def main():
+@app.route('/', methods=['GET', 'POST'])
+def blast_search():
+    # 1:POST요청만 처리
+    if request.method == 'POST':
+        sequence = request.form.get('sequence')
+        result = run_blast_search(sequence)
+        return render_template('main.html', blast_results=result)
+    # 2:POST요청이 없거나 GET요청인 경우
     return render_template('main.html')
+# BlastSearch 수행
+def run_blast_search(sequence):
+    with open('query.fasta', 'w') as file:
+        file.write(f'>query\n{sequence}')
+    subprocess.run(['blastp', '-query', 'query.fasta', '-db', 'prot_db', '-out', 'result.txt'])
+    with open('result.txt', 'r') as file:
+        result = file.read()
+    return result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
